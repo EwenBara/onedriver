21fb71d9eac16423e3b02c62710d528f3e9fb2f7
Jeff Stafford <jeff.stafford@protonmail.com>
2022-07-03 21:31:02 -0400

    add functionality for grabbing multiple drives by id

--- fs/graph/setup_test.go
+++ fs/graph/setup_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+var auth *Auth
+
 func TestMain(m *testing.M) {
 	os.Chdir("../..")
 	f, _ := os.OpenFile("fusefs_tests.log", os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0644)
@@ -16,9 +18,9 @@ func TestMain(m *testing.M) {
 	defer f.Close()
 
 	// auth and log account metadata so we're extra sure who we're testing against
-	auth := Authenticate(".auth_tokens.json", false)
+	auth = Authenticate(".auth_tokens.json", false)
 	user, _ := GetUser(auth)
-	drive, _ := GetDrive(auth)
+	drive, _ := GetDrive("me", auth)
 	log.Info().
 		Str("account", user.UserPrincipalName).
 		Str("type", drive.DriveType).
